7aa491567c8ca1bb5154af0fc06f3e1296be4896
Darkslayer07 <shubhmittal297@gmail.com>
2021-07-20 00:12:51 +0530

    DiceRollApp with image

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,23 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val RollBut: Button = findViewById(R.id.button)
-        val diceTxt: TextView = findViewById(R.id.textView)
-        val diceTxt2: TextView = findViewById(R.id.textView2)
+        val DiceImage: ImageView = findViewById(R.id.imageView)
         RollBut.setOnClickListener {
-            rollDice(6,false,diceTxt,diceTxt2)
-            rollDice(12,true,diceTxt,diceTxt2)
-            winCheck(diceTxt,diceTxt2)
+            rollDice(6,false,DiceImage)
         }
     }
 
-    private fun rollDice(number: Int,SecTxt: Boolean,txt1: TextView,txt2:TextView)
+    private fun rollDice(number: Int,SecTxt: Boolean,diceImg: ImageView)
     {
         val dice = Dice(number)
-        if(SecTxt)
-            txt2.text = dice.roll().toString()
-        else
-            txt1.text = dice.roll().toString()
+        loadImage(diceImg,dice.roll())
+    }
 
+    private fun loadImage(diceImg: ImageView, num:Int)
+    {
+        when(num){
+            1 -> diceImg.setImageResource(R.drawable.dice_1)
+            2 -> diceImg.setImageResource(R.drawable.dice_2)
+            3 -> diceImg.setImageResource(R.drawable.dice_3)
+            4 -> diceImg.setImageResource(R.drawable.dice_4)
+            5 -> diceImg.setImageResource(R.drawable.dice_5)
+            6 -> diceImg.setImageResource(R.drawable.dice_6)
+        }
     }
     private fun winCheck(txt1: TextView,txt2:TextView) {
         if (txt1.text == txt2.text) {
